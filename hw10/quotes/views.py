fca082b5e1df497400f949f47cd0024aef283363
Andrii Lypak <say.elsword@gmail.com>
2024-03-18 16:10:42 +0200

    comm/first_upload
    KeyError: 'bson'

---
+++ hw10/quotes/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.core.paginator import Paginator
+
+
+# Create your views here.
+from .utils import get_mongodb
+
+
+def main(request):
+    db = get_mongodb()
+    quotes = db.quotes.find()
+    per_page = 10
+    paginator = Paginator(list(quotes), per_page)
+    quotes_on_page = paginator.page(page)
+    return render(request, 'quotes/index.html', context={'quotes': quotes_on_page})
